8367ba4c67b6c0864aead33fc9faaf26f182e475
Igor Carmona <igorcarmonawork@gmail.com>
2023-02-02 19:32:29 -0300

    02/02/2023 19:32 --> revisão e novas aulas

---
+++ praticando_e_avançando/rev-declaration_assignment.js
@@ -0,0 +1,11 @@
+// variables and data types:
+
+// Declaration (declaração)
+
+var name
+
+// assignment (Atribuição de valores)
+name = "Igor"
+
+// que tipo de dado foi colocado na variável
+console.log(typeof name)
